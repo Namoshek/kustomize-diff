4a825eb072758469d419c975416fb591b02200ea
Marvin Mall <marvin-mall@msn.com>
2023-08-12 20:58:35 +0200

    Add test for CreateDiffForManifestFiles()

--- kubernetes/manifest_differ_test.go
+++ kubernetes/manifest_differ_test.go
@@ -4,6 +4,51 @@ import (
 	"testing"
 )
 
+func TestCreateDiffForManifestFilesReturnsCorrectResult(t *testing.T) {
+	oldManifest := "apiVersion: apps/v1\nkind: Deployment\nmetadata:\n  name: backend\n  namespace: my-namespace\nspec:\n  replicas: 1\n" +
+				   "---\n" +
+				   "apiVersion: v1\nkind: Service\nmetadata:\n  name: backend-headless\n  namespace: my-namespace\nspec:\n  clusterIP: None" +
+				   "---\n" +
+				   "apiVersion: v1\nkind: Service\nmetadata:\n  name: backend\n  namespace: my-namespace\nspec:\n  type: ClusterIP"
+	newManifest := "apiVersion: apps/v1\nkind: Deployment\nmetadata:\n  name: backend\n  namespace: my-namespace\nspec:\n  replicas: 2\n" +
+				   "---\n" +
+				   "apiVersion: v1\nkind: Service\nmetadata:\n  name: backend-headless\n  namespace: my-namespace\nspec:\n  clusterIP: None" +
+				   "---\n" +
+				   "apiVersion: v1\nkind: Service\nmetadata:\n  name: backend\n  namespace: my-namespace\nspec:\n  type: NodePort"
+
+	diffs, err := CreateDiffForManifestFiles(&oldManifest, &newManifest)
+
+	if err != nil || len(diffs) != 2 {
+		t.Fatal("Diff of files should contain diff of all changed manifests")
+	}
+
+	expectedDiff1 := ` apiVersion: apps/v1
+ kind: Deployment
+ metadata:
+   name: backend
+   namespace: my-namespace
+ spec:
+-  replicas: 1
++  replicas: 2`
+
+	if diffs[0].Diff != expectedDiff1 {
+		t.Fatal("Diff should show changes if manifest was altered. Diff:\n" + diffs[0].Diff)
+	}
+
+	expectedDiff2 := ` apiVersion: v1
+ kind: Service
+ metadata:
+   name: backend
+   namespace: my-namespace
+ spec:
+-  type: ClusterIP
++  type: NodePort`
+
+	if diffs[1].Diff != expectedDiff2 {
+		t.Fatal("Diff should show changes if manifest was altered. Diff:\n" + diffs[1].Diff)
+	}
+}
+
 func TestCreateDiffForManifestsReturnsContentIfManifestsAreIdentical(t *testing.T) {
 	manifest := Manifest{
 		ApiVersion: "v1",
